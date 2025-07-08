2fbfb6c4e301d66c0c420e6aeec082d6be6a3fd5
Thierry Santos <ThierryPitela@hotmail.com>
2025-07-07 21:43:18 -0300

    feat: included galley filter section and grid of places with Rating start logic

--- src/app/gallery/gallery/gallery.component.ts
+++ src/app/gallery/gallery/gallery.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Category } from '../../category/category';
+import { Place } from '../../places/place';
+import { CategoryService } from '../../category/category.service';
+import { PlaceService } from '../../places/place.service';
 
 @Component({
   selector: 'app-gallery',
@@ -7,5 +11,27 @@ import { Component } from '@angular/core';
   styleUrl: './gallery.component.scss'
 })
 export class GalleryComponent {
+  categories: Category[] = [];
+  places: Place[] = [];
 
+  constructor(
+    private categoryService: CategoryService,
+    private placeService: PlaceService) { }
+
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe({
+      next: (categories) => {
+        this.categories = categories;
+      },
+    });
+    this.placeService.getPlaces().subscribe({
+      next: (places) => {
+        this.places = places;
+      },
+    });
+  }
+
+  getRating(rating: number): string {
+    return '★'.repeat(rating) + '☆'.repeat(5 - rating);
+  }
 }
